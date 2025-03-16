9b9fc6bf9d71415ca0f3d49609de668c1785f3c8
Eduardo Maia <emaiax@users.noreply.github.com>
2025-03-16 01:43:00 -0300

    added some vscode configs

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -3,10 +3,40 @@
   programs.vscode = {
     enable = true;
 
-    userSettings = {
-        # This property will be used to generate settings.json:
+    profiles = {
+      default = {
+        extensions = with pkgs.vscode-marketplace; [
+          brettm12345.nixfmt-vscode
+          esbenp.prettier-vscode
+          jnoortheen.nix-ide
+          teabyii.ayu	
+          vscodevim.vim
+        ];
+
+        # https://code.visualstudio.com/docs/getstarted/keybindings#_advanced-customization
+        keybindings = [
+          { key = "cmd+shift+c"; command = "copyRelativeFilePath"; }
+          { key = "cmd+shift+s"; command = "workbench.action.files.saveAll"; }
+        ];
+
         # https://code.visualstudio.com/docs/getstarted/settings#_settingsjson
-        "editor.formatOnSave" = true;
+        userSettings = {
+          # general settings
+          #
+          "breadcrumbs.filePath" = "off";
+          "editor.formatOnSave" = true;
+          "telemetry.telemetryLevel" = "off";
+          "workbench.colorTheme" = "Ayu Mirage Bordered";
+
+          # editor
+          "editor.minimap.enabled" = false;
+          "editor.tabSize" = 2;
+          "explorer.confirmDelete" = false;
+          "explorer.confirmDragAndDrop" = false;
+
+          # "[nix]"."editor.defaultFormatter" = "nixfmt";
+        };
+      };
     };
   };
 }
