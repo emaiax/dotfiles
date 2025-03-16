3a05ef1d1873172dfcff679ae5adc3e86f568a69
Eduardo Maia <emaiax@users.noreply.github.com>
2025-03-16 03:26:18 -0300

    refactor

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -30,15 +30,17 @@
           # general settings
           #
           "breadcrumbs.filePath" = "off";
-          "editor.formatOnSave" = true;
-          "telemetry.telemetryLevel" = "off";
-          "workbench.colorTheme" = "Ayu Mirage Bordered";
 
-          # editor
+          "editor.formatOnSave" = true;
           "editor.minimap.enabled" = false;
           "editor.tabSize" = 2;
+
           "explorer.confirmDelete" = false;
           "explorer.confirmDragAndDrop" = false;
+
+          "telemetry.telemetryLevel" = "off";
+
+          "workbench.colorTheme" = "Ayu Mirage Bordered";
 
           "[nix]" = {
             "editor.defaultFormatter" = "brettm12345.nixfmt-vscode";
