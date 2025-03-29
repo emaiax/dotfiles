4bafc7e60e038b907a374ba740b51b59a7116dee
Eduardo Maia <emaiax@users.noreply.github.com>
2025-03-29 20:08:07 -0300

    fix(iterm2): need to support `unsupported systems` for iterm2

--- hosts/darwin/packages.nix
+++ hosts/darwin/packages.nix
@@ -3,6 +3,10 @@
   # install darwin-specific packages via nixpkgs
   # if package can't be found in nixpkgs, use homebrew
   #
+
+  # iterm2 is available in nixpkgs, but unsupported
+  nixpkgs.config.allowUnsupportedSystem = true;
+
   environment.systemPackages = with pkgs; [
     arc-browser
     iterm2
