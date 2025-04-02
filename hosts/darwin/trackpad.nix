111006b7d974cf3b646aad885f486ede24a46b51
Eduardo Maia <emaiax@users.noreply.github.com>
2025-04-02 20:51:32 -0300

    feat(darwin): extracted trackpad settings

---
+++ hosts/darwin/trackpad.nix
@@ -0,0 +1,20 @@
+{ }:
+{
+  system = {
+    defaults = {
+      trackpad = {
+        Clicking = true; # Whether to enable trackpad tap to click
+        Dragging = true; # Whether to enable tap-to-drag
+
+        # Whether to enable trackpad right click
+        TrackpadRightClick = true;
+
+        # Whether to enable three finger drag
+        TrackpadThreeFingerDrag = true;
+
+        # 0 to disable three finger tap, 2 to trigger Look up & data detectors
+        TrackpadThreeFingerTapGesture = 0;
+      };
+    };
+  };
+}
--- hosts/dudupro.nix
+++ hosts/dudupro.nix
@@ -2,23 +2,6 @@
 {
   imports = [
     ./darwin/security/pam-touch-id.nix
+    ./darwin/trackpad.nix
   ];
-
-  system = {
-    defaults = {
-      trackpad = {
-        Clicking = true; # Whether to enable trackpad tap to click
-        Dragging = true; # Whether to enable tap-to-drag
-
-        # Whether to enable trackpad right click
-        TrackpadRightClick = true;
-
-        # Whether to enable three finger drag
-        TrackpadThreeFingerDrag = true;
-
-        # 0 to disable three finger tap, 2 to trigger Look up & data detectors
-        TrackpadThreeFingerTapGesture = 0;
-      };
-    };
-  };
 }
