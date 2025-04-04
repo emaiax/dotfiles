084a2179fff60739d52970f134797211dc815483
Eduardo Maia <emaiax@users.noreply.github.com>
2025-04-03 23:59:32 -0300

    fix(zsh): add asdf to PATH

--- modules/user/shell/zsh.nix
+++ modules/user/shell/zsh.nix
@@ -1,10 +1,14 @@
-{ ... }:
+{ config, pkgs, ... }:
 {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
 
     defaultKeymap = "viins"; # emacs, vicmd, viins
+
+    initExtra = ''
+      export PATH="$PATH:${config.home.homeDirectory}/.asdf/shims" # Add asdf binaries to PATH
+    '';
 
     shellAliases = {
       cat = "bat";
