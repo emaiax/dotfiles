d2850fcc17742822dab7fc0e852777fbbff438d3
Eduardo Maia <emaiax@users.noreply.github.com>
2025-04-04 20:32:59 -0300

    feat(darwin): finder settings

--- hosts/darwin/finder.nix
+++ hosts/darwin/finder.nix
@@ -3,13 +3,69 @@
   system = {
     defaults = {
       finder = {
+        # keep folders on top when sorting by name on the desktop
+        _FXSortFoldersFirstOnDesktop = true;
+
+        # show the full POSIX filepath in the window title
         _FXShowPosixPathInTitle = false;
-        FXEnableExtensionChangeWarning = false;
-        FXPreferredViewStyle = "Nlsv";
+
+        # Show all file extensions in Finder
         AppleShowAllExtensions = true;
+
+        # Show hidden files in Finder
         AppleShowAllFiles = true;
-        ShowPathbar = false;
+
+        # Change the default search scope, use "SCcf" to default to current folder
+        # The default is unset ("This Mac")
+        #
+        FXDefaultSearchScope = "SCcf";
+
+        # show warnings when change the file extension of files
+        FXEnableExtensionChangeWarning = false;
+
+        # Change the default finder view.
+        #
+        # "icnv" = Icon view
+        # "Nlsv" = List view
+        # "clmv" = Column View
+        # "Flwv" = Gallery View The default is icnv.
+        #
+        FXPreferredViewStyle = "Nlsv";
+
+        # remove items in the trash after 30 days
+        FXRemoveOldTrashItems = true;
+
+        # Change the default folder shown in Finder windows, "Other" corresponds to the value of NewWindowTargetPath
+        # The default is unset ("Recents")
+        #
+        # null or one of "Computer", "OS volume", "Home", "Desktop", "Documents", "Recents", "iCloud Drive", "Other"
+        #
+        NewWindowTarget = "Home";
+        #
+        # Sets the URI to open when NewWindowTarget is "Other".
+        # Spaces and similar characters must be escaped.
+        # If the value is invalid, Finder will open your home directory.
+        # Example: "file:///Users/foo/long%20cat%20pics".
+        #
+        # NewWindowTargetPath = "~/";
+
+        # Allow quitting of the Finder. The default is false.
+        QuitMenuItem = false;
+
+        # Show path breadcrumbs in finder windows. The default is false.
+        ShowPathbar = true;
+
+        # Show status bar at bottom of finder windows with item/disk space stats. The default is false.
         ShowStatusBar = true;
+
+        # Show external disks on desktop. The default is true.
+        ShowExternalHardDrivesOnDesktop = true;
+
+        # Show connected servers on desktop. The default is false.
+        ShowMountedServersOnDesktop = true;
+
+        # Show removable media (CDs, DVDs and iPods) on desktop. The default is true.
+        ShowRemovableMediaOnDesktop = true;
       };
 
       NSGlobalDomain = {
