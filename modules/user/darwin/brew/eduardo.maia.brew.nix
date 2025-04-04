0f81ae1d029bf48a23e21acf6d0d6d53b09420c8
Eduardo Maia <eduardo.maia@therealreal.com>
2025-04-04 11:30:37 -0300

    fix(brew): work brew setup

--- modules/user/darwin/brew/eduardo.maia.brew.nix
+++ modules/user/darwin/brew/eduardo.maia.brew.nix
@@ -1,6 +1,6 @@
 # this module is responsible for configuring homebrew and managing homebrew packages
 # and should be imported by the darwin module and not by the home-manager module.
-# 
+#
 { ... }:
 {
   homebrew = {
@@ -9,16 +9,21 @@
     # nix-homebrew is handling homebrew updates
     global.autoUpdate = true;
 
-    onActivation.autoUpdate = true;
-    onActivation.upgrade = true;
-    onActivation.cleanup = "zap";
+    onActivation = {
+      autoUpdate = true;
+      upgrade = true;
+      cleanup = "zap";
+
+      extraFlags = [ "--force" ];
+    };
 
     brews = [
       "openssl@1.1" # openssl@3: (libev) kqueue kevent: Bad file descriptor
+
       "coreutils"
       "gnupg"
       "gnutls"
-      
+
       # rs-deps
       #
       "cmake"
@@ -28,33 +33,45 @@
       "readline"
       "ruby-build"
       "shared-mime-info"
-      
+
       # databases and dev languages
       #
-      "mysql@8.0"
       "opensearch"
-      "postgresql@14"
       "redis"
+
+      {
+        name = "mysql@8.0";
+        link = true;
+        start_service = true;
+        restart_service = "changed";
+      }
+
+      {
+        name = "postgresql@14";
+        link = true;
+        start_service = true;
+        restart_service = "changed";
+      }
     ];
 
     casks = [
       # applications
       #
-      "1password-cli"     # 1password cli
-      "1password"         # 1password
-      "contexts"          # context menu for mac
+      "1password-cli" # 1password cli
+      "1password" # 1password
+      "contexts" # context menu for mac
       "domzilla-caffeine" # caffeine
-      "github"            # github desktop for mac
-      "logi-options+"     # logitech options
-      "obsidian"          # note taking
-      "rectangle"         # window manager
-      "slack"             # communication
-      "tableplus"         # database management
-      "the-unarchiver"    # unarchiver
+      "github" # github desktop for mac
+      "logi-options+" # logitech options
+      "obsidian" # note taking
+      "rectangle" # window manager
+      "slack" # communication
+      "tableplus" # database management
+      "the-unarchiver" # unarchiver
 
       # dependencies
       #
-      "temurin"           # java 17
+      "temurin" # java 17
     ];
   };
 }
