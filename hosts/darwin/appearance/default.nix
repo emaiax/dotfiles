7245f444ca49843342e53ebeb69f7863a51a2036
Eduardo Maia <emaiax@users.noreply.github.com>
2025-04-17 11:38:25 -0300

    fix(darwin): added derivation to change ui accent colors

--- hosts/darwin/appearance/default.nix
+++ hosts/darwin/appearance/default.nix
@@ -5,6 +5,23 @@
   ...
 }:
 let
+  # https://github.com/NixOS/nixpkgs/blob/master/doc/stdenv/platform-notes.chapter.md
+  sendUIEvents = pkgs.stdenv.mkDerivation {
+    name = "send-ui-events";
+    version = "unstable";
+    dontUnpack = true;
+
+    src = ./send-ui-events;
+
+    # ensure swift/apple-sdk is available and can run the script
+    buildInputs = [ pkgs.apple-sdk ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp $src $out/bin/send-ui-events
+    '';
+  };
+
   colorAccents = import ./accent-colors.nix;
 
   # https://mynixos.com/nix-darwin/option/system.defaults.NSGlobalDomain.AppleInterfaceStyle
@@ -43,6 +60,8 @@ in
   };
 
   config = {
+    environment.systemPackages = [ sendUIEvents ];
+
     system.defaults = {
       NSGlobalDomain = {
         # this requires the user to logout and login again.
@@ -76,6 +95,8 @@ in
 
       setupAccentColor.text = ''
         echo "setting up appearance accent colors to ${config.system.defaults.appearance.accent}..."
+
+        ${sendUIEvents}/bin/send-ui-events
       '';
 
     };
