b13dacb9136c4a9261668c307d0a2a40a40a0772
Eduardo Maia <emaiax@users.noreply.github.com>
2025-04-17 13:51:37 -0300

    chore(darwin): extracted sendUIEvents to derivation module

--- modules/system/darwin/appearance/default.nix
+++ modules/system/darwin/appearance/default.nix
@@ -1,28 +1,7 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, ... }:
 let
-  # https://github.com/NixOS/nixpkgs/blob/master/doc/stdenv/platform-notes.chapter.md
-  sendUIEvents = pkgs.stdenv.mkDerivation {
-    name = "send-ui-events";
-    version = "unstable";
-    dontUnpack = true;
-
-    src = ./send-ui-events;
-
-    # ensure swift/apple-sdk is available and can run the script
-    buildInputs = [ pkgs.apple-sdk ];
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cp $src $out/bin/send-ui-events
-    '';
-  };
-
   colorAccents = import ./accent-colors.nix;
+  sendUIEvents = import ./send-ui-events.nix;
 
   # https://mynixos.com/nix-darwin/option/system.defaults.NSGlobalDomain.AppleInterfaceStyle
   # Set to 'Dark' to enable dark mode, or leave unset for normal mode.
@@ -84,13 +63,13 @@ in
       setupInterfaceMode.text = ''
         echo "setting up appearance mode to ${config.system.defaults.appearance.mode}..."
 
-        osascript -e "
-          tell application \"System Events\" \
-          to tell appearance preferences \
+        osascript -e '
+          tell application "System Events"
+          to tell appearance preferences
           to set dark mode to ${
             if config.system.defaults.appearance.mode == "dark" then "true" else "false"
           }
-        "
+        '
       '';
 
       setupAccentColor.text = ''
---
+++ modules/system/darwin/appearance/send-ui-events.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+{
+  # https://github.com/NixOS/nixpkgs/blob/master/doc/stdenv/platform-notes.chapter.md
+  #
+  sendUIEvents = pkgs.stdenv.mkDerivation {
+    name = "send-ui-events";
+    version = "unstable";
+    dontUnpack = true;
+
+    src = ./send-ui-events;
+
+    # ensure swift/apple-sdk is available and can run the script
+    buildInputs = [ pkgs.apple-sdk ];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp $src $out/bin/send-ui-events
+    '';
+  };
+}
