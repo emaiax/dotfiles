b7b5d9eecd234c0e75ac0b1ce149d826d25dcda2
Eduardo Maia <emaiax@users.noreply.github.com>
2025-04-17 13:58:28 -0300

    fix(darwin): broken bin derivation

--- modules/system/darwin/appearance/send-ui-events.nix
+++ modules/system/darwin/appearance/send-ui-events.nix
@@ -1,20 +1,18 @@
+# https://github.com/NixOS/nixpkgs/blob/master/doc/stdenv/platform-notes.chapter.md
+#
 { pkgs, ... }:
-{
-  # https://github.com/NixOS/nixpkgs/blob/master/doc/stdenv/platform-notes.chapter.md
-  #
-  sendUIEvents = pkgs.stdenv.mkDerivation {
-    name = "send-ui-events";
-    version = "unstable";
-    dontUnpack = true;
+pkgs.stdenv.mkDerivation {
+  name = "send-ui-events";
+  version = "unstable";
+  dontUnpack = true;
 
-    src = ./send-ui-events;
+  src = ./send-ui-events;
 
-    # ensure swift/apple-sdk is available and can run the script
-    buildInputs = [ pkgs.apple-sdk ];
+  # ensure swift/apple-sdk is available and can run the script
+  buildInputs = [ pkgs.apple-sdk ];
 
-    installPhase = ''
-      mkdir -p $out/bin
-      cp $src $out/bin/send-ui-events
-    '';
-  };
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src $out/bin/send-ui-events
+  '';
 }
