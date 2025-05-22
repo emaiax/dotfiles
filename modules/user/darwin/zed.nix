a0e0f78e80dcb7540aab86ff5c3e96a816fdfaf5
Eduardo Maia <emaiax@users.noreply.github.com>
2025-05-21 22:58:18 -0300

    feat(zed): added configs

--- modules/user/darwin/zed.nix
+++ modules/user/darwin/zed.nix
@@ -2,5 +2,32 @@
 {
   programs.zed-editor = {
     enable = true;
+
+    extensions = [
+      "elixir"
+      "javascript"
+      "just"
+      "ruby"
+      "rust"
+      "typescript"
+      "zig"
+    ];
+
+    userSettings = {
+      ui_font_size = 16;
+      buffer_font_size = 16;
+
+      theme = {
+        mode = "system";
+        light = "One Light";
+        dark = "One Dark";
+      };
+
+      vim_mode = true;
+
+      telemetry = {
+        metrics = false;
+      };
+    };
   };
 }
