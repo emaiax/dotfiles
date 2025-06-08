6cbd143c896e8c880b6151995dc37bd36fe62f5d
emaiax <emaiax@fastmail.com>
2025-06-08 18:27:16 -0300

    fix(darwin): comment broken package

--- modules/user/darwin/packages.nix
+++ modules/user/darwin/packages.nix
@@ -8,7 +8,7 @@
     code-cursor # cursor ide
     docker # docker cli
     docker-compose # docker compose cli
-    lmstudio # local and open-source LLMs
+    # lmstudio # local and open-source LLMs
     uv # python package manager
   ];
 }
