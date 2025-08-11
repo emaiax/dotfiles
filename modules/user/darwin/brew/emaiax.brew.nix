fe06bf4dd10f58a4c784fd5333952b243be97209
Eduardo Maia <emaiax@users.noreply.github.com>
2025-08-10 22:00:54 -0300

    feat(brew): added `cursor-cli`

--- modules/user/darwin/brew/emaiax.brew.nix
+++ modules/user/darwin/brew/emaiax.brew.nix
@@ -35,6 +35,7 @@
 
       # Development
       #
+      "cursor-cli" # cursor ai cli
       "docker-desktop" # docker desktop for mac
       # "tableplus" # database management
       "github" # github desktop for mac
