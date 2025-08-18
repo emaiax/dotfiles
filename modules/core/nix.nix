89198141e1fe456d3b00408d534b305e9f55bee2
Eduardo Maia <emaiax@users.noreply.github.com>
2025-08-18 16:00:31 -0300

    fix: add `arc-browser` to `permittedInsecurePackages`

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -25,8 +25,15 @@
   };
 
   nixpkgs = {
-    config.allowUnfree = true;
-    config.allowUnsupportedSystem = true;
+    config = {
+      allowBroken = false;
+      allowUnfree = true;
+      allowUnsupportedSystem = true;
+
+      # some unmaintained packages are allowed to be installed
+      #
+      permittedInsecurePackages = lib.attrNames [ pkgs.arc-browser ];
+    };
 
     hostPlatform = host.arch;
 
