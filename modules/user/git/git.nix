9dfd2cae2f5cc335d75bab4d9e0d6892e1ff3334
Eduardo Maia <emaiax@users.noreply.github.com>
2025-10-25 23:53:05 -0300

    git: refactor warnings

--- modules/user/git/git.nix
+++ modules/user/git/git.nix
@@ -14,97 +14,81 @@
   programs.git = {
     enable = true;
 
-    delta.enable = true;
     lfs.enable = true;
     ignores = lib.splitString "\n" (builtins.readFile ./gitignore-global);
 
-    userName = "Eduardo Maia";
-    userEmail = "emaiax@users.noreply.github.com";
+    settings = {
+      user = {
+        name = "Eduardo Maia";
+        email = "emaiax@users.noreply.github.com";
+        signingKey = "~/.ssh/github.pub";
+      };
 
-    aliases = {
-      a = "add .";
-
-      # commit and ammend
-      ci = "commit -m";
-      cia = "commit -s --amend --no-edit";
-
-      co = "checkout";
-
-      # grab a change from a branch and replay it
-      cp = "cherry-pick -x";
-
-      # diff
-      df = "diff"; # unstaged changes
-      dc = "diff --cached"; # staged changes
-
-      # log
-      lg = "log --oneline --decorate -20";
-
-      # please = "push --force-with-lease";
-
-      st = "status";
-
-      undo = "reset --soft HEAD^1"; # undo last commit
-      unstage = "restore --staged"; # unstage changes
-    };
-    extraConfig = {
       # sign commits with ssh key
       # https://docs.github.com/en/authentication/managing-commit-signature-verification/telling-git-about-your-signing-key#telling-git-about-your-ssh-key
       #
       commit.gpgSign = true;
+      gpg.allowedSignersFile = "~/.ssh/allowed_signers";
       gpg.format = "ssh";
-      gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
       tag.gpgSign = true;
-      user.signingkey = "~/.ssh/github.pub";
 
-      # core = {
-      #   editor = "nvim";
-      #   compression = -1;
-      #   autocrlf = "input";
-      #   whitespace = "trailing-space,space-before-tab";
-      #   precomposeunicode = true;
-      # };
-      advice = {
-        addEmptyPathspec = false;
-      };
-      apply = {
-        whitespace = "nowarn";
-      };
+      # other settings
+      #
+      advice.addEmptyPathspec = false;
+      apply.whitespace = "nowarn";
+      format.signOff = true;
+      help.autocorrect = 1;
+      init.defaultBranch = "main";
+      log.showSignature = true;
+      rerere.enabled = true;
+      submodule.fetchJobs = 4;
+
       color = {
         diff = "auto";
         status = "auto";
         branch = "auto";
         ui = true;
       };
-      format = {
-        signOff = true;
-      };
+
       grep = {
         extendRegexp = true;
         lineNumber = true;
       };
-      help = {
-        autocorrect = 1;
-      };
-      log = {
-        showSignature = false;
-      };
-      init = {
-        defaultBranch = "main";
-      };
+
       pull = {
-        # ff = "only";
         rebase = true;
       };
+
       push = {
         autoSetupRemote = true;
         default = "simple";
       };
-      rerere = {
-        enabled = true;
-      };
-      submodule = {
-        fetchJobs = 4;
+
+      alias = {
+        a = "add .";
+
+        # commit and ammend
+        ci = "commit -m";
+        cia = "commit -s --amend --no-edit";
+
+        co = "checkout";
+
+        # grab a change from a branch and replay it
+        cp = "cherry-pick -x";
+
+        # diff
+        df = "diff"; # unstaged changes
+        dc = "diff --cached"; # staged changes
+
+        # log
+        lg = "log --oneline --decorate -20";
+
+        # please = "push --force-with-lease";
+
+        st = "status";
+
+        undo = "reset --soft HEAD^1"; # undo last commit
+        unstage = "restore --staged"; # unstage changes
       };
     };
   };
