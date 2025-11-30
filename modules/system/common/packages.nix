d55f1d103811d4c8246ae15cb44a7ba8ed07c55e
Eduardo Maia <emaiax@users.noreply.github.com>
2025-11-30 16:27:53 -0300

    chore: fmt

--- modules/system/common/packages.nix
+++ modules/system/common/packages.nix
@@ -4,17 +4,17 @@
   # It installs the packages in the system environment (available for all users).
   #
   environment.systemPackages = with pkgs; [
-    asdf-vm          # programming languages version manager
-    btop             # better top
-    curl             # command line tool for transferring data with URLs
-    jq               # command-line JSON processor
-    httpie           # command line tool for HTTP requests
-    neovim           # vim on steroids
+    asdf-vm # programming languages version manager
+    btop # better top
+    curl # command line tool for transferring data with URLs
     # go               # go programming language
+    jq # command-line JSON processor
+    httpie # command line tool for HTTP requests
+    neovim # vim on steroids
     nixfmt-rfc-style # format nix files in RFC style
-    tlrc             # command line tool for managing TL;DR pages
-    unixtools.watch  # watch command for running a program periodically
-    watchexec        # run a command when files change
-    wget             # command line tool for downloading files from the web
+    tlrc # command line tool for managing TL;DR pages
+    unixtools.watch # watch command for running a program periodically
+    watchexec # run a command when files change
+    wget # command line tool for downloading files from the web
   ];
 }
